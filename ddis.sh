a45d5beed54f418ebc33fdd6caef5c62aca5046a
Igor S <igoshein@gmail.com>
2019-07-26 21:57:59 +0300

    Update ddis.sh

--- ddis.sh
+++ ddis.sh
@@ -6,9 +6,6 @@ sudo apt-get -y install wget
 ## Gnome terminal
 sudo apt-get -y install gnome-terminal
 
-## Terminator
-sudo apt-get -y install terminator
-
 ## Google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get -y install ./google-chrome-stable_current_amd64.deb
